bd6ee1a501f3cc9fc49ad2dcfa73996daedb66dc
zamfiroiu <alin.zamfiroiu@csie.ase.ro>
2022-10-24 15:12:50 +0300

    Add files via upload

---
+++ ExempluPractic_24Octombrie.cpp
@@ -0,0 +1,35 @@
+﻿//#include<iostream>
+//
+//using std::string;
+//
+//class Cana {
+//private:
+//	int cantitate;
+//	string culoare;
+//	string material;
+//
+//public:
+//	int metoda() { //int metoda(Cana* this)
+//		this->cantitate = 100;
+//		this->culoare = "Rosie";
+//		this->material = "Plastic";
+//	}
+//
+//	void modificaCuloare(string culoare) {
+//		this->culoare = culoare;
+//	}
+//
+//	void functieNormala();
+//};
+//
+//void Cana::functieNormala() {
+//	std::cout << "Apel functie";
+//}
+//
+//void main() {
+//	Cana cana1;
+//	Cana cana2;
+//	Cana cana3;
+//	cana1.metoda();
+//
+//}
